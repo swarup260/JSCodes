009d4ce6d9d5d8432a87ce34f799d51fcfbff11c
swarup das <swarup260@gmail.com>
2020-04-01 00:14:12 +0530

    user controller , routes and helper function added

--- Todo/backend/api/helpers/function.helper.js
+++ Todo/backend/api/helpers/function.helper.js
@@ -1,7 +1,23 @@
 
 const jwt = require('jsonwebtoken');
-const config = require('../config')
+const config = require('../config');
+const userModel = require('../models/User.model');
 
-exports.getToken = (data) => {
-    return  jwt.sign({ username : data.username , email : data.email}, config.JWT_SECRET, { expiresIn: config.JWT_EXPIRES });
+exports.getToken = data =>  jwt.sign({ username : data.username , email : data.email}, config.JWT_SECRET, { expiresIn: config.JWT_EXPIRES });
+
+exports.userExists = async data => {
+    const user = await userModel.findOne({email : data.email});
+    if (!user) {
+        throw new Error("user not found");
+    }
+    
+    if(!user.status){
+        throw new Error("user is  disable");
+    }
+    return await user;
 }
+
+// { username: 'swarup260',
+//   email: 'swarup260@gmail.com',
+//   iat: 1585678135,
+//   exp: 1585681735 }
