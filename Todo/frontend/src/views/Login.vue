604086b7f9fd1fd44130938926e8ac63d983019e
swarup <swarup260@gmail.com>
2020-04-04 17:55:27 +0530

    login & sign view added

---
+++ Todo/frontend/src/views/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+      <b-form @submit="onSubmit">
+       <b-form-input
+          v-model="formData.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+       <b-form-input
+          v-model="formData.password"
+          type="password"
+          required
+          placeholder="Enter password"
+        ></b-form-input>
+        <b-button type="submit" variant="primary">Submit</b-button>
+      </b-form>
+      <b-link href="/signup">SignUp</b-link>
+  </div>
+</template>
+<script>
+import { mapActions } from 'vuex'
+export default {
+  data () {
+    return {
+      formData: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    ...mapActions(['login']),
+    onSubmit: function (event) {
+      event.preventDefault()
+      this.login(this.formData)
+    }
+  }
+
+}
+</script>
+<style scoped>
+</style>
---
+++ Todo/frontend/src/views/SignUp.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+      <b-form @submit="onSubmit">
+       <b-form-input
+          v-model="formData.username"
+          type="text"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+       <b-form-input
+          v-model="formData.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+       <b-form-input
+          v-model="formData.password"
+          type="password"
+          required
+          placeholder="Enter password"
+        ></b-form-input>
+        <b-button type="submit" variant="primary">Submit</b-button>
+      </b-form>
+  </div>
+</template>
+<script>
+import { mapActions } from 'vuex'
+export default {
+  data () {
+    return {
+      formData: {
+        username: '',
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    ...mapActions(['signup']),
+    onSubmit: function (event) {
+      event.preventDefault()
+      this.signup(this.formData)
+    }
+  }
+
+}
+</script>
+<style scoped>
+</style>
