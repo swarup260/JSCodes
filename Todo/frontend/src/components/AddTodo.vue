8fa49b87305409e6d48f9fb798e5fde592279de2
swarup <swarup260@gmail.com>
2020-04-04 17:57:41 +0530

    addTodo and view Todo component added

---
+++ Todo/frontend/src/components/AddTodo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-form inline @submit="onSubmit">
+      <b-input
+        id="inline-form-input-name"
+        class="mb-2 mr-sm-2 mb-sm-0"
+        v-model="formData.taskName"
+        placeholder="Enter Todo"
+      ></b-input>
+      <b-form-datepicker id="example-datepicker" v-model="formData.deadline" class="mb-2"></b-form-datepicker>
+      <b-form-checkbox
+        id="checkbox-1"
+        v-model="formData.status"
+        name="checkbox-1"
+        unchecked-value="not_accepted"
+      ></b-form-checkbox>
+      <b-button type="submit" variant="primary">Add</b-button>
+    </b-form>
+  </div>
+</template>
+<script>
+import { mapActions } from 'vuex'
+export default {
+  data () {
+    return {
+      formData: {
+        taskName: '',
+        status: false,
+        deadline: Date.now(),
+        isComplete: false
+      }
+    }
+  },
+  methods: {
+    ...mapActions(['addTodo']),
+    onSubmit: function (event) {
+      event.preventDefault()
+      this.addTodo(this.formData)
+    }
+  }
+}
+</script>
