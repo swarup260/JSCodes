7be8598d15242a79f534f21f75b0bdba0e3c48e7
swarup das <swarup260@gmail.com>
2020-04-06 23:46:26 +0530

    content center vertically & horizontally design changed

--- Todo/frontend/src/views/Login.vue
+++ Todo/frontend/src/views/Login.vue
@@ -1,33 +1,47 @@
 <template>
 <!-- eslint-disable -->
-<b-container class="bv-example-row my-4 align-self-center">
-    <b-row align-h="center">
-        <b-col  align-v="center" cols="6">
-            <b-card no-body class="p-6">
-                <b-card-body>
-                    <b-form @submit="onSubmit">
-                        <h1 class="text-center">Login</h1>
-                        <p class="text-center">Sign In to your account</p>
-                        <b-input-group class="mb-4">
-                            <b-form-input type="email" class="form-control" placeholder="Enter email" autocomplete="email" v-model="formData.email"  />
-                        </b-input-group>
-                        <b-input-group class="mb-4">
-                            <b-form-input type="password" class="form-control" placeholder="Enter password" autocomplete="password" v-model="formData.password"  />
-                        </b-input-group>
-                        <b-row>
-                            <b-col cols="6" class="text-left">
-                                <b-button type="submit" variant="dark" class="px-3">Login</b-button>
-                            </b-col>
-                            <b-col cols="6" class="text-right">
-                                <b-button href="/signup" variant="link" class="px-0">Not Have Account?</b-button>
-                            </b-col>
-                        </b-row>
-                    </b-form>
-                </b-card-body>
-            </b-card>
-        </b-col>
-    </b-row>
-</b-container>
+  <div class="parent">
+    <div class="child">
+       <b-card no-body>
+          <b-card-body>
+            <b-form @submit="onSubmit">
+         <h1 class="text-center">Login</h1>
+         <p class="text-center">Sign In to your account</p>
+         <b-row>
+          <b-col cols="12">
+            <b-input-group class="mb-4">
+            <b-form-input 
+              type="email" 
+              class="form-control" 
+              placeholder="Enter email" 
+              autocomplete="email" 
+              v-model="formData.email"  />
+            </b-input-group>
+          </b-col>
+          <b-col cols="12">
+           <b-input-group class="mb-4">
+            <b-form-input 
+              type="password" 
+              class="form-control" 
+              placeholder="Enter password"
+              autocomplete="password" 
+              v-model="formData.password"  />
+            </b-input-group>
+          </b-col>
+         </b-row>
+         <b-row>
+           <b-col cols="6" class="text-left">
+             <b-button type="submit" variant="dark" class="px-3">Login</b-button>
+           </b-col>
+           <b-col cols="6" class="text-right">
+              <b-button href="/signup" variant="link" class="px-0">Not Have Account?</b-button>
+           </b-col>
+         </b-row>
+       </b-form>
+          </b-card-body>
+        </b-card>
+    </div>
+  </div>
 </template>
 <script>
 import { mapActions, mapGetters } from 'vuex'
@@ -52,5 +66,15 @@ export default {
 }
 </script>
 <style scoped>
-
+.parent {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.child {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
--- Todo/frontend/src/views/SignUp.vue
+++ Todo/frontend/src/views/SignUp.vue
@@ -1,33 +1,52 @@
 <template>
     <!-- eslint-disable -->
-    <b-container class="bv-example-row my-4 align-self-center">
-        <b-row align-h="center">
-            <b-col  align-v="center" cols="6">
-                <b-card no-body class="p-4">
-                    <b-card-body>
-                        <b-form @submit="onSubmit">
-                            <h1 class="text-center">Sign Up</h1>
-                            <p class="text-center">Sign Up to your account</p>
-                            <b-input-group class="mb-3">
-                                <b-form-input type="text" class="form-control" placeholder="Enter username" autocomplete="username" v-model="formData.username"  />
-                            </b-input-group>
-                            <b-input-group class="mb-3">
-                                <b-form-input type="text" class="form-control" placeholder="Enter email" autocomplete="username" v-model="formData.email"  />
-                            </b-input-group>
-                            <b-input-group class="mb-3">
-                                <b-form-input type="text" class="form-control" placeholder="Enter password" autocomplete="username" v-model="formData.password"  />
-                            </b-input-group>
-                            <b-row>
-                                <b-col cols="6" class="text-left">
-                                    <b-button variant="dark" class="px-3">SignUp</b-button>
-                                </b-col>
-                            </b-row>
-                        </b-form>
-                    </b-card-body>
-                </b-card>
-            </b-col>
-        </b-row>
-    </b-container>
+    <div class="parent">
+      <div class="child">
+        <b-card no-body>
+          <b-card-body>
+            <b-form @submit="onSubmit">
+         <h1 class="text-center">Sign Up</h1>
+         <p class="text-center">Sign Up to your account</p>
+         <b-row>
+          <b-col cols="12">
+            <b-input-group class="mb-4">
+            <b-form-input 
+              type="username" 
+              class="form-control" 
+              placeholder="Enter username" 
+              autocomplete="username" 
+              v-model="formData.username"  />
+            </b-input-group>
+            <b-input-group class="mb-4">
+            <b-form-input 
+              type="email" 
+              class="form-control" 
+              placeholder="Enter email" 
+              autocomplete="email" 
+              v-model="formData.email"  />
+            </b-input-group>
+          </b-col>
+          <b-col cols="12">
+           <b-input-group class="mb-4">
+            <b-form-input 
+              type="password" 
+              class="form-control" 
+              placeholder="Enter password"
+              autocomplete="password" 
+              v-model="formData.password"  />
+            </b-input-group>
+          </b-col>
+         </b-row>
+         <b-row>
+           <b-col cols="6" class="text-left">
+             <b-button type="submit" variant="dark" class="px-3">SignUp</b-button>
+           </b-col>
+         </b-row>
+       </b-form>
+          </b-card-body>
+        </b-card>
+      </div>
+    </div>
 </template>
 <script>
 import { mapActions } from 'vuex'
@@ -52,4 +71,15 @@ export default {
 }
 </script>
 <style scoped>
+.parent {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.child {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
